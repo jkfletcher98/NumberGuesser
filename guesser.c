0c09fdb70d3038d8bdf46b5c7ba715cd429cf61c
jkfletcher98 <jkfletcher@bsu.edu>
2025-01-21 15:44:45 +0000

    A few tweaks

--- guesser.c
+++ guesser.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 int main(){
 	char userName[20];
 	printf("Hi! What's your name? ");
 	scanf("%s", userName);
 	printf("Nice to meet you, %s! Try to guess what number I'm thinking of. \n", userName);
+
 	srand(time(NULL));
 	int correct = (rand() % 100) + 1;
-	printf("%d", correct);
+	printf("%d \n", correct);
+
 	return 0;
 } // end main
